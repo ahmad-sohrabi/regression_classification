fbcc1c050b1fbbc879e4bbed4981ea18c8c763b5
Ahmad Sohrabi <ahmadsohrabi69@gmail.com>
2023-11-30 08:55:16 +0330

    Initial Code for classification is ok now

---
+++ classification.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import metrics
+from sklearn.model_selection import cross_val_score
+
+data = pd.read_csv("train_classification.csv")
+data.drop(columns=['PassengerId', 'Cabin', 'Name', 'Ticket'], inplace=True)
+
+data.fillna(method='ffill', inplace=True)
+data.dropna(inplace=True)
+column_names = data.columns.to_list()
+result = data.applymap(np.isreal)
+non_numeric_columns = np.array([])
+for i in range(len(column_names)):
+    array = np.array(result[column_names[i]])
+    if array[0] == False:
+        non_numeric_columns = np.append(non_numeric_columns, column_names[i])
+for i in range(len(non_numeric_columns)):
+    data[[non_numeric_columns[i]]] = data[[non_numeric_columns[i]]].apply(LabelEncoder().fit_transform)
+
+input_x = data.iloc[:, 1:].values
+target = data.iloc[:, 0].values
+input_train, input_test, target_train, target_test = train_test_split(input_x, target, test_size=0.20, random_state=42)
+logistic_reg = LogisticRegression(max_iter=10000)
+logistic_reg.fit(input_train, target_train)
+predictions = logistic_reg.predict(input_test)
+score = logistic_reg.score(input_test, target_test)
+confusion_matrix = metrics.confusion_matrix(target_test, predictions)
+plt.figure(1).set_figheight(35)
+plt.figure(1).set_figwidth(20)
+plt.subplot(211)
+sns.heatmap(confusion_matrix, annot=True, fmt=".3f", linewidths=.5, square=True, cmap='Blues_r')
+plt.ylabel('Actual label')
+plt.xlabel('Predicted label')
+all_sample_title = 'Accuracy Score: {0}'.format(score)
+plt.title(all_sample_title, size=15)
+
+logistic_reg2 = LogisticRegression()
+
+max_k = 10
+log_reg_mean_scores = np.array([])
+for k in range(2, max_k + 1):
+    cv = KFold(n_splits=k)
+    model = LogisticRegression(max_iter=10000)
+    scores = cross_val_score(model, input_x, target, scoring='accuracy', cv=cv, n_jobs=-1)
+    log_reg_mean_scores = np.append(log_reg_mean_scores, np.mean(scores))
+plt.subplot(212)
+plt.plot(range(2, max_k + 1), log_reg_mean_scores, linewidth=5)
+plt.title("Compare different K in K-Fold")
+plt.xlabel("K")
+plt.ylabel("Mean of Scores")
+plt.show()
--- regression.py
+++ regression.py
@@ -1,51 +1,65 @@
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
 from sklearn.linear_model import Lasso
-from sklearn.model_selection import cross_validate
 from sklearn.model_selection import KFold
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import LabelEncoder
 
 
+def kfold_mean_error(model, input, output, k):
+    kf = KFold(n_splits=k)
+    error = []
+    for train_index, test_index in kf.split(input):
+        X_train, X_test, y_train, y_test = input[train_index], input[test_index], \
+            output[train_index], output[test_index]
+
+        model.fit(X_train, y_train)
+
+        y_pred = model.predict(X_test)
+
+        error.append(mean_squared_error(y_test, y_pred))
+
+    return np.sqrt(np.mean(error))
+
+
 data = pd.read_csv("train_regression.csv")
-data.drop(columns=['Alley', 'Id', 'PoolQC', 'Fence', 'MiscFeature', 'FireplaceQu','LotFrontage'], inplace=True)
-data.fillna(method ='ffill', inplace = True)
-data.dropna(inplace = True)
+data.drop(columns=['Alley', 'Id', 'PoolQC', 'Fence', 'MiscFeature', 'FireplaceQu', 'LotFrontage'], inplace=True)
+data.fillna(method='ffill', inplace=True)
+data.dropna(inplace=True)
 column_names = data.columns.to_list()
 result = data.applymap(np.isreal)
 non_numeric_columns = np.array([])
 for i in range(len(column_names)):
-   array = np.array(result[column_names[i]])
-   if array[0] == False:
-      non_numeric_columns = np.append(non_numeric_columns, column_names[i])
-for i in range(len( non_numeric_columns )):
-   data[[non_numeric_columns[i]]] = data[[non_numeric_columns[i]]].apply(LabelEncoder().fit_transform)
+    array = np.array(result[column_names[i]])
+    if array[0] == False:
+        non_numeric_columns = np.append(non_numeric_columns, column_names[i])
+for i in range(len(non_numeric_columns)):
+    data[[non_numeric_columns[i]]] = data[[non_numeric_columns[i]]].apply(LabelEncoder().fit_transform)
 
-input = data.iloc[:,:-1].values
-target = data.iloc[:,-1].values
-input_train, input_test, target_train, target_test = train_test_split(input, target,test_size=0.20,random_state=42)
+input_x = data.iloc[:, :-1].values
+target = data.iloc[:, -1].values
+
+input_train, input_test, target_train, target_test = train_test_split(input_x, target, test_size=0.20, random_state=42)
 lin_reg = LinearRegression()
+
 lin_reg.fit(input_train, target_train)
 train_predicted_price_lin = lin_reg.predict(input_train)
 train_rmse_lin = np.sqrt(mean_squared_error(target_train, train_predicted_price_lin))
-print(train_rmse_lin)
+print(f"linear train error {train_rmse_lin}")
 
 test_predicted_price_lin = lin_reg.predict(input_test)
 test_rmse_lin = np.sqrt(mean_squared_error(target_test, test_predicted_price_lin))
-print(test_rmse_lin)
+print(f"linear test error {test_rmse_lin}")
 
 lasso_reg = Lasso(alpha=1000)
 lasso_reg.fit(input_train, target_train)
 train_predicted_price_lasso = lasso_reg.predict(input_train)
 train_rmse_lasso = np.sqrt(mean_squared_error(target_train, train_predicted_price_lasso))
-print(train_rmse_lasso)
+print(f"lasso train error {train_rmse_lasso}")
 
 test_predicted_price_lasso = lasso_reg.predict(input_test)
 test_rmse_lasso = np.sqrt(mean_squared_error(target_test, test_predicted_price_lasso))
@@ -66,22 +80,22 @@ price = train_data["SalePrice"]
 area = train_data["LotArea"]
 rooms = train_data["TotRmsAbvGrd"]
 
-price.fillna(method ='ffill', inplace = True)
-area.fillna(method ='ffill', inplace = True)
-rooms.fillna(method ='ffill', inplace = True)
+price.fillna(method='ffill', inplace=True)
+area.fillna(method='ffill', inplace=True)
+rooms.fillna(method='ffill', inplace=True)
 
-price.dropna(inplace = True)
-area.dropna(inplace = True)
-rooms.dropna(inplace = True)
+price.dropna(inplace=True)
+area.dropna(inplace=True)
+rooms.dropna(inplace=True)
 
-area_train, area_test, price_train, price_test = train_test_split(area, price,test_size=0.30,random_state=42)
+area_train, area_test, price_train, price_test = train_test_split(area, price, test_size=0.30, random_state=42)
 
 plt.scatter(area_train, price_train)
 plt.title("Area vs Price")
 plt.ylabel("Price (Dollars)")
 plt.xlabel("Area (Square Feet)")
 
-rooms_train, rooms_test, price_train2, price_test2 = train_test_split(rooms, price,test_size=0.30,random_state=42)
+rooms_train, rooms_test, price_train2, price_test2 = train_test_split(rooms, price, test_size=0.30, random_state=42)
 
 plt.scatter(rooms_train, price_train2)
 plt.title("Rooms vs Price")
@@ -108,9 +122,9 @@ rooms_reg.fit(rooms_train, price_train2)
 
 predicted_price_area = area_reg.predict(area_train)
 
-plt.scatter(area_train, price_train, color ='b', label="Real Data")
+plt.scatter(area_train, price_train, color='b', label="Real Data")
 
-plt.plot(area_train, predicted_price_area, color ='k', label="Linear Model")
+plt.plot(area_train, predicted_price_area, color='k', label="Linear Model")
 
 plt.title("Price vs Area")
 plt.ylabel("Price (Dollars)")
@@ -120,26 +134,26 @@ plt.show()
 
 predicted_prices_rooms = rooms_reg.predict(rooms_train)
 
-plt.scatter(rooms_train, price_train2, color ='b', label="Real Data")
+plt.scatter(rooms_train, price_train2, color='b', label="Real Data")
 
-plt.plot(rooms_train, predicted_prices_rooms, color ='k', label="Linear Model")
+plt.plot(rooms_train, predicted_prices_rooms, color='k', label="Linear Model")
 plt.title("Price vs Rooms")
 plt.ylabel("Price (Dollars)")
 plt.xlabel("Number of Rooms")
 plt.legend()
 plt.show()
 
-area_ridge = Ridge(alpha = 3)
+area_ridge = Ridge(alpha=3)
 area_ridge.fit(area_train, price_train)
 
-rooms_ridge = Ridge(alpha = 1)
+rooms_ridge = Ridge(alpha=1)
 rooms_ridge.fit(rooms_train, price_train2)
 
 predicted_area_ridge = area_ridge.predict(area_train)
 
-plt.scatter(area_train, price_train, color ='b', label="Real Data")
+plt.scatter(area_train, price_train, color='b', label="Real Data")
 
-plt.plot(area_train, predicted_area_ridge, color ='k', label="Ridge Model")
+plt.plot(area_train, predicted_area_ridge, color='k', label="Ridge Model")
 plt.title("Price vs Area (Ridge Model)")
 plt.ylabel("Price (Dollars)")
 plt.xlabel("Area (Square Feet)")
@@ -148,9 +162,9 @@ plt.show()
 
 predicted_prices_rooms_ridge = rooms_ridge.predict(rooms_train)
 
-plt.scatter(rooms_train, price_train2, color ='b', label="Real Data")
+plt.scatter(rooms_train, price_train2, color='b', label="Real Data")
 
-plt.plot(rooms_train, predicted_prices_rooms_ridge,color ='k', label="Ridge Model")
+plt.plot(rooms_train, predicted_prices_rooms_ridge, color='k', label="Ridge Model")
 
 plt.title("Price vs Rooms(Ridge Model)")
 plt.ylabel("Price (Dollars)")
@@ -158,17 +172,17 @@ plt.xlabel("Number of Rooms")
 plt.legend()
 plt.show()
 
-area_lasso = Lasso(alpha = 1)
+area_lasso = Lasso(alpha=1)
 area_lasso.fit(area_train, price_train)
 
-rooms_lasso = Lasso(alpha = 1)
+rooms_lasso = Lasso(alpha=1)
 rooms_lasso.fit(rooms_train, price_train2)
 
 predicted_price_area_lasso = area_lasso.predict(area_train)
 
-plt.scatter(area_train, price_train, color ='b', label="Real Data")
+plt.scatter(area_train, price_train, color='b', label="Real Data")
 
-plt.plot(area_train, predicted_price_area_lasso, color ='k', label="Lasso Model")
+plt.plot(area_train, predicted_price_area_lasso, color='k', label="Lasso Model")
 
 plt.title("Price vs Area (Lasso Model)")
 plt.ylabel("Price (Dollars)")
@@ -178,9 +192,9 @@ plt.show()
 
 predicted_price_rooms_lasso = rooms_lasso.predict(rooms_train)
 
-plt.scatter(rooms_train, price_train2, color ='b', label="Real Data")
+plt.scatter(rooms_train, price_train2, color='b', label="Real Data")
 
-plt.plot(rooms_train, predicted_price_rooms_lasso, color ='k', label="Lasso Model")
+plt.plot(rooms_train, predicted_price_rooms_lasso, color='k', label="Lasso Model")
 
 plt.title("Rooms vs Price (Lasso Model)")
 plt.xlabel("Price (Dollars)")
@@ -198,28 +212,27 @@ rooms = np.array(rooms).reshape(-1, 1)
 price = np.array(price).reshape(-1, 1)
 
 area_test_lin_price_pred = area_reg.predict(area_test)
-area_rmse_lin = np.sqrt(mean_squared_error(price_test,area_test_lin_price_pred))
+area_rmse_lin = np.sqrt(mean_squared_error(price_test, area_test_lin_price_pred))
 print(area_rmse_lin)
 
 rooms_test_lin_price_pred = rooms_reg.predict(rooms_test)
-rooms_rmse_lin = np.sqrt(mean_squared_error(price_test2,rooms_test_lin_price_pred))
+rooms_rmse_lin = np.sqrt(mean_squared_error(price_test2, rooms_test_lin_price_pred))
 print(rooms_rmse_lin)
 
-
 area_test_ridge_price_pred = area_ridge.predict(area_test)
-area_rmse_ridge = np.sqrt(mean_squared_error(price_test,area_test_ridge_price_pred))
+area_rmse_ridge = np.sqrt(mean_squared_error(price_test, area_test_ridge_price_pred))
 print(area_rmse_ridge)
 
 rooms_test_ridge_price_pred = rooms_ridge.predict(rooms_test)
-rooms_rmse_ridge = np.sqrt(mean_squared_error(price_test2,rooms_test_ridge_price_pred))
+rooms_rmse_ridge = np.sqrt(mean_squared_error(price_test2, rooms_test_ridge_price_pred))
 print(rooms_rmse_ridge)
 
 area_test_lasso_price_pred = area_lasso.predict(area_test)
-area_rmse_lasso = np.sqrt(mean_squared_error(price_test,area_test_lasso_price_pred))
+area_rmse_lasso = np.sqrt(mean_squared_error(price_test, area_test_lasso_price_pred))
 print(area_rmse_lasso)
 
 rooms_test_lasso_price_pred = rooms_lasso.predict(rooms_test)
-rooms_rmse_lasso = np.sqrt(mean_squared_error(price_test2,rooms_test_lin_price_pred))
+rooms_rmse_lasso = np.sqrt(mean_squared_error(price_test2, rooms_test_lin_price_pred))
 print(rooms_rmse_lasso)
 
 labels = ["Linear", "Ridge", "Lasso"]
@@ -228,46 +241,30 @@ plt.bar(labels, errors)
 plt.title("Area RMSE Comparison")
 plt.ylabel("RMSE")
 
-def kfold_mean_error(model, input, output, k):
-   kf = KFold(n_splits=k)
-   error = []
-   for train_index, test_index in kf.split(input):
-        X_train, X_test, y_train, y_test = input[train_index], input[test_index],\
-                                           output[train_index], output[test_index]
-
-
-        model.fit(X_train, y_train)
-
-        y_pred = model.predict(X_test)
-
-        error.append(mean_squared_error(y_test, y_pred))
-
-   return np.sqrt(np.mean(error))
-
 max_k = 10
 area_lin_mean_error = np.array([])
-for k in range(2,max_k+1):
-   err =kfold_mean_error(area_reg, area, price, k)
-   area_lin_mean_error = np.append(area_lin_mean_error, err)
-plt.plot(range(2, max_k+1), area_lin_mean_error)
+for k in range(2, max_k + 1):
+    err = kfold_mean_error(area_reg, area, price, k)
+    area_lin_mean_error = np.append(area_lin_mean_error, err)
+plt.plot(range(2, max_k + 1), area_lin_mean_error)
 plt.title("Compare different K in K-Fold")
 plt.xlabel("K")
 plt.ylabel("RMSE")
 
 area_ridge_mean_error = np.array([])
-for k in range(2,max_k+1):
-   err =kfold_mean_error(area_ridge, price, area, k)
-   area_ridge_mean_error = np.append(area_ridge_mean_error, err)
-plt.plot(range(2, max_k+1), area_ridge_mean_error)
+for k in range(2, max_k + 1):
+    err = kfold_mean_error(area_ridge, price, area, k)
+    area_ridge_mean_error = np.append(area_ridge_mean_error, err)
+plt.plot(range(2, max_k + 1), area_ridge_mean_error)
 plt.title("Compare different K in K-Fold")
 plt.xlabel("K")
 plt.ylabel("RMSE")
 
 area_lasso_mean_error = np.array([])
-for k in range(2,max_k+1):
-   err =kfold_mean_error(area_lasso, area, price, k)
-   area_lasso_mean_error = np.append(area_lasso_mean_error, err)
-plt.plot(range(2, max_k+1), area_lasso_mean_error)
+for k in range(2, max_k + 1):
+    err = kfold_mean_error(area_lasso, area, price, k)
+    area_lasso_mean_error = np.append(area_lasso_mean_error, err)
+plt.plot(range(2, max_k + 1), area_lasso_mean_error)
 plt.title("Compare different K in K-Fold")
 plt.xlabel("K")
 plt.ylabel("RMSE")
